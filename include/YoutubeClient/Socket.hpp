fb9417bdd5299690d74b11e9641ed3d870d52f06
laynia <vc_mikuu@hotmail.com>
2024-09-09 22:56:01 +0200

    Update Socket.hpp

--- include/YoutubeClient/Socket.hpp
+++ include/YoutubeClient/Socket.hpp
@@ -1,11 +1,11 @@
-#pragma once
+// #pragma once
 
-#include <string>
+//#include <string>
 
-std::string getHostIP(const std::string& host);
+//std::string getHostIP(const std::string& host);
 
-std::string fetchHTML(const std::string& host, const std::string& page);
+//std::string fetchHTML(const std::string& host, const std::string& page);
 
-bool parseChatMessage(const std::string& html, std::string& username, std::string& message);
+//bool parseChatMessage(const std::string& html, std::string& username, std::string& message);
 
-void continuouslyCheckForMessages(const std::string& videoID, void (*onNewMessage)(const std::string&, const std::string&));
+//void continuouslyCheckForMessages(const std::string& videoID, void (*onNewMessage)(const std::string&, const std::string&));
