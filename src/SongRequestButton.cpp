47da429a85af8046c2fa8a14b6d1c27c0bf302ec
laynia <vc_mikuu@hotmail.com>
2024-10-03 13:03:10 +0200

    Update SongRequestButton.cpp

--- src/SongRequestButton.cpp
+++ src/SongRequestButton.cpp
@@ -22,7 +22,7 @@ using namespace TMPro;
 // this is only for the song request button
 
 void CreateSongRequestButton() {
-    UnityEngine::UI::HorizontalLayoutGroup* layout = BSML::Lite::CreateHorizontalLayoutGroup();
+    UnityEngine::UI::HorizontalLayoutGroup* layout = BSML::Lite::CreateHorizontalLayoutGroup(layout->get_transform());
     Button* button = BSML::Lite::CreateUIButton(layout->get_transform(), "", "Chat Requests", nullptr);
     button->get_transform()->set_localScale({2.0f, 3.0f, 2.0f});
     button->set_interactable(false);
